133bc42038b3129ade2e52a6c6253164c2b5f035
Mário Hatalčík <xhatalc@fi.muni.cz>
2025-10-12 16:05:50 +0200

    add copilot chat

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -128,4 +128,14 @@ return {
   {
     "github/copilot.vim",
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  },
 }
